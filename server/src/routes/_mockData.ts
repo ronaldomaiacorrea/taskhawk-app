6112144cd2b5b7f371f4f7debb9c5731ea9f7cda
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2024-09-13 21:17:45 -0700

    Changing mock data

--- server/src/routes/_mockData.ts
+++ server/src/routes/_mockData.ts
@@ -23,7 +23,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Compile and prepare the quarterly financial and performance report.',
 		creationDate: new Date('2023-06-17T04:40:15'),
-		dueDate: new Date('2024-04-07T05:01:56'),
+		dueDate: new Date('2023-04-07T05:01:56'),
 		priority: 'Low',
 		status: Status.TO_DO,
 	},
@@ -33,7 +33,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Submit receipts and complete forms for expense reimbursement for the recent business trip.',
 		creationDate: new Date('2023-04-01T15:32:08'),
-		dueDate: new Date('2024-06-27T06:44:11'),
+		dueDate: new Date('2023-06-27T06:44:11'),
 		priority: 'Low',
 		status: Status.OVERDUE,
 	},
@@ -43,7 +43,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Identify and fix the bug in the user login system that has been reported.',
 		creationDate: new Date('2023-04-29T03:35:39'),
-		dueDate: new Date('2024-01-13T20:04:36'),
+		dueDate: new Date('2023-01-13T20:04:36'),
 		priority: 'High',
 		status: Status.COMPLETED,
 	},
@@ -53,7 +53,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Attend the project meeting to discuss progress, issues, and next steps for Project X.',
 		creationDate: new Date('2023-07-01T13:00:29'),
-		dueDate: new Date('2024-10-30T22:28:50'),
+		dueDate: new Date('2023-10-30T22:28:50'),
 		priority: 'High',
 		status: Status.OVERDUE,
 	},
@@ -63,7 +63,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Review and make adjustments to the client presentation scheduled for next week.',
 		creationDate: new Date('2023-01-24T21:26:58'),
-		dueDate: new Date('2024-04-13T00:49:00'),
+		dueDate: new Date('2023-04-13T00:49:00'),
 		priority: 'Medium',
 		status: Status.COMPLETED,
 	},
@@ -73,7 +73,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Research potential new markets for product expansion and present findings.',
 		creationDate: new Date('2023-11-14T15:05:22'),
-		dueDate: new Date('2024-07-20T06:10:41'),
+		dueDate: new Date('2023-07-20T06:10:41'),
 		priority: 'Medium',
 		status: Status.IN_PROGRESS,
 	},
@@ -83,7 +83,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Draft and publish a blog post outlining the latest product features.',
 		creationDate: new Date('2023-10-12T19:12:08'),
-		dueDate: new Date('2024-06-18T02:20:18'),
+		dueDate: new Date('2023-06-18T02:20:18'),
 		priority: 'Low',
 		status: Status.COMPLETED,
 	},
@@ -93,7 +93,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Update the product documentation to reflect new features and updates.',
 		creationDate: new Date('2023-02-24T04:44:31'),
-		dueDate: new Date('2024-03-23T03:35:26'),
+		dueDate: new Date('2023-03-23T03:35:26'),
 		priority: 'Medium',
 		status: Status.IN_PROGRESS,
 	},
@@ -103,7 +103,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Create wireframes and mockups for the new feature being added to the product.',
 		creationDate: new Date('2023-08-01T15:10:09'),
-		dueDate: new Date('2024-09-10T01:18:46'),
+		dueDate: new Date('2023-09-10T01:18:46'),
 		priority: 'High',
 		status: Status.BLOCKED,
 	},
@@ -113,7 +113,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Review and approve code changes submitted in the pull request by the development team.',
 		creationDate: new Date('2023-03-14T10:24:43'),
-		dueDate: new Date('2024-05-05T11:32:29'),
+		dueDate: new Date('2023-05-05T11:32:29'),
 		priority: 'Low',
 		status: Status.BLOCKED,
 	},
@@ -123,7 +123,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Design and develop a marketing campaign for the upcoming product launch.',
 		creationDate: new Date('2023-07-29T14:22:16'),
-		dueDate: new Date('2024-12-25T17:42:35'),
+		dueDate: new Date('2023-12-25T17:42:35'),
 		priority: 'High',
 		status: Status.OVERDUE,
 	},
@@ -133,7 +133,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Conduct performance reviews for team members and provide constructive feedback.',
 		creationDate: new Date('2023-05-11T05:13:23'),
-		dueDate: new Date('2024-04-16T09:54:48'),
+		dueDate: new Date('2023-04-16T09:54:48'),
 		priority: 'Medium',
 		status: Status.TO_DO,
 	},
@@ -143,7 +143,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Prepare the financial forecast for the next fiscal year, including revenue and expense projections.',
 		creationDate: new Date('2023-09-09T21:24:44'),
-		dueDate: new Date('2024-07-31T22:15:11'),
+		dueDate: new Date('2023-07-31T22:15:11'),
 		priority: 'High',
 		status: Status.COMPLETED,
 	},
@@ -153,7 +153,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Organize a team-building event to improve collaboration and morale within the team.',
 		creationDate: new Date('2023-12-03T12:45:20'),
-		dueDate: new Date('2024-11-11T20:22:50'),
+		dueDate: new Date('2023-11-11T20:22:50'),
 		priority: 'Low',
 		status: Status.TO_DO,
 	},
@@ -163,7 +163,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Review contracts from suppliers to ensure they meet company standards and requirements.',
 		creationDate: new Date('2023-10-13T20:44:44'),
-		dueDate: new Date('2024-01-09T08:41:37'),
+		dueDate: new Date('2023-01-09T08:41:37'),
 		priority: 'High',
 		status: Status.TO_DO,
 	},
@@ -173,7 +173,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Finalize the budget for Q3, ensuring all projects and departments are accounted for.',
 		creationDate: new Date('2023-10-19T06:45:56'),
-		dueDate: new Date('2024-08-14T07:13:54'),
+		dueDate: new Date('2023-08-14T07:13:54'),
 		priority: 'Medium',
 		status: Status.IN_PROGRESS,
 	},
@@ -183,7 +183,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Develop and test the API integration for the new external service.',
 		creationDate: new Date('2023-08-20T07:24:42'),
-		dueDate: new Date('2024-02-20T14:11:25'),
+		dueDate: new Date('2023-02-20T14:11:25'),
 		priority: 'High',
 		status: Status.COMPLETED,
 	},
@@ -193,7 +193,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Schedule posts for social media platforms based on the marketing calendar.',
 		creationDate: new Date('2023-06-17T16:01:27'),
-		dueDate: new Date('2024-08-18T08:15:19'),
+		dueDate: new Date('2023-08-18T08:15:19'),
 		priority: 'Medium',
 		status: Status.IN_PROGRESS,
 	},
@@ -203,7 +203,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Test the latest software release for bugs and performance issues before deployment.',
 		creationDate: new Date('2023-02-21T19:06:24'),
-		dueDate: new Date('2024-07-19T05:01:04'),
+		dueDate: new Date('2023-07-19T05:01:04'),
 		priority: 'High',
 		status: Status.COMPLETED,
 	},
@@ -213,7 +213,7 @@ export const mockTasks: Task[] = [
 		description:
 			'Plan and coordinate logistics for the company retreat, including venue, travel, and activities.',
 		creationDate: new Date('2023-11-05T18:17:53'),
-		dueDate: new Date('2024-10-17T03:57:37'),
+		dueDate: new Date('2023-10-17T03:57:37'),
 		priority: 'High',
 		status: Status.OVERDUE,
 	},
