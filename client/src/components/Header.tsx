6bbfe349e15af56cb5392688d3342564fb66dbab
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2024-10-07 11:48:16 -0700

    Adding logo in the header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,19 @@
 import { useContext } from 'react';
 import { Button } from 'primereact/button';
 import { DarkModeContext } from '@context/DarkModeProvider';
+import Logo from '../../public/TaskHawk_Logo_Transparent.png';
 
 const Header = () => {
 	const { toggleTheme } = useContext(DarkModeContext);
 
 	return (
-		<header className="sticky top-0 z-50 bg-gradient-to-r from-slate-800 to-teal-800/90 px-8 shadow-md shadow-teal-500/50 backdrop-blur-md">
-			<div className="mx-auto h-16 flex flex-row justify-between items-center text-white">
-				<div className="flex gap-2">
-					<div className="font-bold hidden sm:block">Logo</div>
-					<div className="text-lg font-bold text-white">TaskHawk</div>
+		<header className="sticky top-0 z-50 bg-gradient-to-r from-slate-800 to-teal-800/90 px-8 shadow-md shadow-teal-500/50 backdrop-blur-md py-1S">
+			<div className="mx-auto h-auto flex flex-row justify-between items-center text-white">
+				<div className="flex items-center gap-2">
+					<div className="font-bold hidden sm:block">
+						<img src={Logo} width={100} alt="Task Hawk logo"/>
+					</div>
+					<div className="text-2xl font-bold text-white">Task Hawk</div>
 				</div>
 				<div className="flex gap-2">
 					<Button
