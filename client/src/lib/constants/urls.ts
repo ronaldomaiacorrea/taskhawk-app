e1d89a78744d2587317d9494268936faa6df9b97
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2024-10-28 13:05:41 -0700

    Fixing URL issue

--- client/src/lib/constants/urls.ts
+++ client/src/lib/constants/urls.ts
@@ -1,4 +1,4 @@
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
-export const TASKS_API_URL = `${BASE_URL}/tasks`;
-export const CATEGORIES_API_URL = `${BASE_URL}/categories`;
+export const TASKS_API_URL = `${BASE_URL}/api/tasks`;
+export const CATEGORIES_API_URL = `${BASE_URL}/api/categories`;
