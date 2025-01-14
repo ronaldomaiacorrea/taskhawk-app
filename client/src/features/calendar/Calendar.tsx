147e48f40f8cd17524b284364923ccacbf5bd120
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2025-01-14 11:51:03 -0800

    Adding tasks to Calendar

--- client/src/features/calendar/Calendar.tsx
+++ client/src/features/calendar/Calendar.tsx
@@ -1,21 +1,41 @@
-// Import { backgroundColors } from '@constants';
-import frLocale from '@fullcalendar/core/locales/fr-ca';
-import ptLocale from '@fullcalendar/core/locales/pt-br';
-import zhLocale from '@fullcalendar/core/locales/zh-cn';
+import { ErrorMessage, Spinner } from '@common';
+import type { EventApi, EventClickArg } from '@fullcalendar/core';
+import allLocales from '@fullcalendar/core/locales-all';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import { useTranslations } from '@hooks/useTranslations';
-
+import { useTasks } from '@queries';
+import { Status } from '@shared/types';
 import { Tooltip } from 'primereact/tooltip';
 import PageTitle from 'src/common/PageTitle';
+import { backgroundColors } from 'src/lib/constants/themes';
 
 const Calendar = () => {
   const { t, i18n } = useTranslations();
-  // Const { data: tasks } = useMonthTasks();
+  const { data: tasks = [], isLoading, isError, error } = useTasks();
 
-  // const [initialView, setInitialView] = useState('dayGridMonth');
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <ErrorMessage error={error} />;
+  }
+
+  const filteredTasks = tasks.filter(
+    (task) => task.status !== Status.COMPLETED,
+  );
+
+  const handleEdit = (info: EventClickArg) => {
+    window.console.log(info.event.title);
+  };
+
+  const statusOrder: Record<string, number> = {
+    Overdue: 0,
+    'In progress': 1,
+    'To do': 2,
+  };
 
   return (
     <>
@@ -25,9 +45,9 @@ const Calendar = () => {
       <Tooltip />
       <FullCalendar
         locale={i18n.language}
-        locales={[ptLocale, zhLocale, frLocale]}
+        locales={allLocales}
         headerToolbar={{
-          left: 'prev,next today',
+          left: 'prev,next,today',
           center: 'title',
           right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
         }}
@@ -38,28 +58,57 @@ const Calendar = () => {
         }}
         titleFormat={{ year: 'numeric', month: 'long', day: 'numeric' }}
         plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
-        initialView={'dayGridMonth'}
-        // Events={tasks.map((task) => ({
-        //   title: task.title,
-        //   start: task.creationDate.toISOString(),
-        //   end: task.dueDate.toISOString(),
-        //   description: task.description,
-        //   backgroundColor: backgroundColors[task.status],
-        //   borderColor: backgroundColors[task.status],
-        // }))}
+        initialView="dayGridMonth"
+        events={filteredTasks.map((task) => ({
+          title: task.title,
+          start: new Date(task.creationDate).toISOString(),
+          end: new Date(task.dueDate).toISOString(),
+          description: task.description,
+          backgroundColor: task.status
+            ? (backgroundColors?.[task.status] ?? '')
+            : '',
+          borderColor: task.status
+            ? (backgroundColors?.[task.status] ?? '')
+            : '',
+          status: task.status,
+        }))}
         allDaySlot={false}
-        height={1000}
+        height={1200}
         timeZone="local"
         dayMaxEvents
         editable
+        selectable
         listDayFormat={{ weekday: 'long' }}
         displayEventTime
-        eventOrder="start,title"
         moreLinkClick="popover"
         navLinks
         handleWindowResize
         stickyHeaderDates
-        eventClick={(info) => info.event.title}
+        eventClick={(info) => handleEdit(info)}
+        eventDidMount={(info) => {
+          const description =
+            info.event.extendedProps.description || 'No description';
+          const status = info.event.extendedProps.status || 'No status';
+          info.el.setAttribute('title', `${description} - Status: ${status}`);
+        }}
+        eventOrder={(a, b) => {
+          const eventA = a as EventApi;
+          const eventB = b as EventApi;
+          return (
+            statusOrder[eventA.extendedProps.status] -
+            statusOrder[eventB.extendedProps.status]
+          );
+        }}
+        eventContent={(eventInfo) => (
+          <div>
+            <strong>{eventInfo.timeText}</strong> -{' '}
+            <span>{eventInfo.event.title}</span>
+            <div>{eventInfo.event.extendedProps.description}</div>
+            <div>
+              <div>{`${t('tasks.status')}: ${eventInfo.event.extendedProps.status}`}</div>
+            </div>
+          </div>
+        )}
       />
     </>
   );
